42950b77e092b657255d0b0d6a062c04bee56683
pays0n <63822744+PAYS0N@users.noreply.github.com>
2025-11-13 10:53:14 -0600

    Setup decision path

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
