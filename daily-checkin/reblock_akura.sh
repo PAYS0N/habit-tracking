23f91eefb2eeeeddaedb9c74813732e9d1a60a3b
pays0n <63822744+PAYS0N@users.noreply.github.com>
2026-04-09 21:38:19 -0500

    Flush connections on block

--- daily-checkin/reblock_akura.sh
+++ daily-checkin/reblock_akura.sh
@@ -2,3 +2,4 @@
 # Reblock akura_malice after a video game session expires.
 # Invoked by a transient systemd timer scheduled via schedule_akura_reblock().
 /usr/sbin/ipset del allowed_internet 192.168.22.52
+/usr/sbin/conntrack -D -s 192.168.22.52
